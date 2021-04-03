269ceeb4b81d5391c543855260cea4981d363551
Edilson <silva.edilson@gmail.com>
2021-04-03 12:48:14 -0300

    criar componente de servico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CadastroLoginComponent } from './cadastro-login/cadastro-login.component';
+import { CadastroServicoComponent } from './cadastro-servico/cadastro-servico.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CadastroLoginComponent
+    CadastroLoginComponent,
+    CadastroServicoComponent
   ],
   imports: [
     BrowserModule,
