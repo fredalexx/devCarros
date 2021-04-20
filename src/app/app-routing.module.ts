408e890098a09258bbb2e3e4ad706ed7922f0a5a
Fred <fred.alex@hotmail.com>
2021-04-19 21:23:29 -0300

    Add rotas e tema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { CadastroVeiculoComponent } from './cadastro-veiculo/cadastro-veiculo.component';
+import { CadastroServicoComponent } from './cadastro-servico/cadastro-servico.component';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,6 +10,9 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: CadastroLoginComponent },
   { path: 'login/:codigo', component: CadastroLoginComponent },
+  { path: 'cliente', component: CadastroClienteComponent },
+  { path: 'servico', component: CadastroServicoComponent },
+  { path: 'veiculo', component: CadastroVeiculoComponent },
 ];
 
 @NgModule({
